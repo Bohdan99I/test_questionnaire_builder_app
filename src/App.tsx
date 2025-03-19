2b932977ac4ce965968ed3f84f3237ab67234427
Bohdan Voitov <bohdan.voiyov@gmail.com>
2025-03-19 20:43:56 +0200

    add_pages+fix_app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme";
 import Layout from "./components/Layout";
-
+import QuestionnaireCatalog from "./pages/QuestionnaireCatalog";
+import QuestionnaireBuilder from "./pages/QuestionnaireBuilder";
+import QuestionnaireRun from "./pages/QuestionnaireRun";
 
 function App() {
   return (
@@ -12,7 +14,10 @@ function App() {
       <BrowserRouter>
         <Layout>
           <Routes>
-            ////////////////////////////
+            <Route path="/" element={<QuestionnaireCatalog />} />
+            <Route path="/create" element={<QuestionnaireBuilder />} />
+            <Route path="/edit/:id" element={<QuestionnaireBuilder />} />
+            <Route path="/run/:id" element={<QuestionnaireRun />} />
           </Routes>
         </Layout>
       </BrowserRouter>
